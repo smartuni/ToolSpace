bf7bd1325e8338b63c4d1d5afba8f03bd02e62a3
Eltonno <dragonu1@web.de>
2017-10-26 20:23:10 +0200

    hello folder

--- hello/src/main/java/hello/GreetingController.java
+++ hello/src/main/java/hello/GreetingController.java
@@ -11,15 +11,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GreetingController {
 
-    private static final String template = "Hello, %d!";
+    private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
     @RequestMapping("/greeting")
-    public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
-        model.addAttribute("name", name);
-        return "hello";
-/*        return new Greeting(counter.incrementAndGet(),
+    public Greeting greeting(@RequestParam(value="name", required=false, defaultValue="World") String name) {
+        return new Greeting(counter.incrementAndGet(),
                             String.format(template, name));
-   */ }
+    }
 
 }
