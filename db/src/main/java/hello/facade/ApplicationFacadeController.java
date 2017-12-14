afda961766fe702894b2025e393c940a2f815317
Florian Weiß <dragonu1@web.de>
2017-12-14 13:46:07 +0100

    vsp zwischenstand

--- db/src/main/java/hello/facade/ApplicationFacadeController.java
+++ db/src/main/java/hello/facade/ApplicationFacadeController.java
@@ -108,15 +108,4 @@ class ApplicationFacadeController {
             return new ResponseEntity(HttpStatus.NOT_ACCEPTABLE);
         }
     }
-
-    @RestController()
-    @RequestMapping(path="/api")
-    public class TestController {
-
-        @GetMapping(path = "/hello-world")
-        public String helloWorld(){
-            return "You fool!";
-        }
-
-    }
 }
